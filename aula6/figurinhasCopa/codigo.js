cc721af3f6e074ef5fa7f3f785b363d0a2b56372
laisvidoto1994 <lais.vidoto@gmail.com>
2018-12-20 11:12:33 -0200

    add

---
+++ aula6/figurinhasCopa/codigo.js
@@ -0,0 +1,90 @@
+ window.onload = function()
+{   
+	
+  var figura1 = document.getElementById('figura1').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome1').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";    
+    var mostar    = document.getElementById("done1").innerHTML = menssagem; 
+	var teste     = document.getElementById("principal").innerHTML = menssagem;	
+	
+	function image() 
+	{
+		var img = document.createElement("IMG").src = "imagens/Cristiano Ronaldo.jpg";
+		document.getElementById('image').appendChild(img);
+	}
+	
+  }); 
+ 
+  var figura2 = document.getElementById('figura2').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome2').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done2").innerHTML = menssagem; 
+  });
+
+  var figura3 = document.getElementById('figura3').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome3').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done3").innerHTML = menssagem; 
+  });
+
+  var figura4 = document.getElementById('figura4').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome4').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done4").innerHTML = menssagem; 
+  });
+
+  var figura5 = document.getElementById('figura5').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome5').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done5").innerHTML = menssagem; 
+  });
+  
+  var figura6 = document.getElementById('figura6').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome6').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!"; 
+    var mostar    = document.getElementById("done6").innerHTML = menssagem; 
+  });
+
+  var figura7 = document.getElementById('figura7').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome7').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done7").innerHTML = menssagem; 
+  });
+
+  var figura8 = document.getElementById('figura8').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome8').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done8").innerHTML = menssagem; 
+  });
+
+  
+  var figura9 = document.getElementById('figura9').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome9').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done9").innerHTML = menssagem; 
+  });
+
+  var figura10 = document.getElementById('figura10').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome10').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done10").innerHTML = menssagem; 
+  });
+
+  var figura11 = document.getElementById('figura11').addEventListener ("click", function(e)
+  { 
+    var nomes     = document.getElementById('nome11').htmlFor; 
+    var menssagem =  nomes + ", é meu jogador favorito!";
+    var mostar    = document.getElementById("done11").innerHTML = menssagem; 
+  });
+
+}
